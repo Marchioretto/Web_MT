60b9f97ddd4ca7dd2ee8c57af5974cd2519b176c
gustamarch <gustamarchi@gmail.com>
2024-04-07 18:23:49 -0300

    Web MT Test

---
+++ Web_MT.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas
+
+
+st.header('MT ACESSORIA DE MARKETING')
+st.image('images/mtcapa.jpg')
+
+col1, col2 = st.columns(2)
+
+df = pandas.read_csv("data.csv")
+
+with col1:
+    for index, row in df[:3].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col2:
+    for index, row in df[3:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
